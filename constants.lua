283170b41ae64f8165b3e1d34e12f26b9be668a9
Hariyama Harine <hariyama.harine@gmail.com>
2021-09-05 00:36:31 +0900

    add: initialize Game setting

---
+++ constants.lua
@@ -0,0 +1,12 @@
+local _m = {}
+
+_m._W = display.contentWidth
+_m._H = display.contentHeight
+
+_m.wallWidth = 20
+_m.floorHeight = 30
+
+_m.colorWhite = { 244/255, 244/255, 244/255 }
+_m.colorBlack = { 33/255, 33/255, 33/255 }
+
+return _m
